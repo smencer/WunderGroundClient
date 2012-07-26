96de7e775a9d63e5d83603686c3b914f58254e38
Scott Mencer <smencer@gmail.com>
2012-07-25 22:11:31 -0400

    Setting up basic Client and Responses classes
    The Responses class is filled in with what is needed to get current
    conditions and forecast responses.

---
+++ WunderGroundClient/WunderGroundClient.Conditions.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp;
+using WunderGroundClient.Model;
+
+namespace WunderGroundClient
+{
+    public partial class WunderGroundClient
+    {
+        private RestRequest getBaseRequest()
+        {
+            var request = new RestRequest(Method.GET)
+            {
+                Resource = "/{key}/{features}/q/{query}.json"
+            };
+
+            return request;
+        }
+
+        public WunderGroundResponse GetCurrentConditionsByZip(string zipCode)
+        {
+            var request = getBaseRequest();
+
+            request.AddParameter("key", _clientId, ParameterType.UrlSegment);
+            request.AddParameter("features", "conditions", ParameterType.UrlSegment);
+            request.AddParameter("query", zipCode, ParameterType.UrlSegment);
+
+            return ExectuteGet<WunderGroundResponse>(request);
+        }
+    }
+}
---
+++ WunderGroundClient/WunderGroundClient.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using RestSharp;
+
+namespace WunderGroundClient
+{
+    public partial class WunderGroundClient
+    {
+        private const string BaseUrl = "http://api.wunderground.com/api/";
+
+        private readonly RestClient _client;
+        private readonly string _clientId;
+
+        public WunderGroundClient(string clientId)
+            : this(clientId, new RestClient(BaseUrl))
+        {
+        }
+
+        public WunderGroundClient(string clientId, RestClient restClient)
+        {
+            _client = restClient;
+            _clientId = clientId;
+        }
+
+        private T ExectuteGet<T>(RestRequest request) where T : new()
+        {
+            var response = _client.Execute<T>(request);
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return default(T);
+            }
+
+            return response.Data;
+        }
+    }
+}
